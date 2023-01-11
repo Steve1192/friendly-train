33750f9451e3d37a75eb7a33b83b9dc1f3334a77
Steve1192 <stevesantner@gmail.com>
2023-01-11 09:59:46 -0500

    reformatting after adding the feature

--- index.js
+++ index.js
@@ -30,57 +30,52 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     
-    playerGuessesContainer.addEventListener('submit', function(e) 
-    {
-        e.preventDefault();
-        let playerGuess = e.target.querySelector('input').value;     
+playerGuessesContainer.addEventListener('submit', function(e) 
+{
+    e.preventDefault();
+    let playerGuess = e.target.querySelector('input').value;     
           if (playerGuess == teamsArray[rando])
           { outputMessage.innerHTML = "YOU GUESSED IT! It was: " + playerGuess }
         
           else
          { i ++
-            outputMessage.innerHTML = "YOU'RE A BAD BASKETBALL FAN"
-            let wrongGuess = document.createElement('li')
+          outputMessage.innerHTML = "YOU'RE A BAD BASKETBALL FAN"
+          let wrongGuess = document.createElement('li')
             wrongGuess.innerHTML = playerGuess
-            guessesContainer.appendChild(wrongGuess)
-                      if (i == 5)
-                      {outputMessage.innerHTML = "The team " + playersArray[rando] + " played for most in his career was the " + teamsArray[rando] + "."; }
+          guessesContainer.appendChild(wrongGuess)
+                if (i == 5)
+                    {outputMessage.innerHTML = "The team " + playersArray[rando] + " played for most in his career was the " + teamsArray[rando] + "."; }
 
-                      else if (i == 6) 
-                      {window.location.reload();}
+                else if (i == 6) 
+                    {window.location.reload();}
           }
-        e.target.reset(); 
-    });
+    e.target.reset(); 
+});
       
       title.addEventListener("mouseover", function() 
       {
           if (title.style.color === "black") 
           { title.style.color = "red";} 
-
           else if (title.style.color === "red") 
           {title.style.color = "blue";} 
-          
           else {title.style.color = "black";}
       });
 
       renderAPlayer.addEventListener("click", function(e)
       {
         if (blinking == false) 
-        {renderAPlayer.style.color = 'black'
-        stevesInterval = setInterval(function() {
-          if (renderAPlayer.style.color === "black")
-          {renderAPlayer.style.color = 'orange'}
-          else if (renderAPlayer.style.color === 'orange') 
-          {renderAPlayer.style.color = 'black';}
-                                }, 1000)
-          blinking = true;
-         }
+              {renderAPlayer.style.color = 'black'
+              stevesInterval = setInterval(function() {
+                if (renderAPlayer.style.color === "black")
+                {renderAPlayer.style.color = 'orange'}
+                else if (renderAPlayer.style.color === 'orange') 
+                {renderAPlayer.style.color = 'black';}
+                                      }, 1000)
+                blinking = true;
+              }
          else {
-          blinking = false;
-          clearInterval(stevesInterval);
-         }
+                blinking = false;
+                clearInterval(stevesInterval);
+              }
         
       })
-
-
-    
